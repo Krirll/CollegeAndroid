4b28b64d4cb42f9640a161c3557296b5557ddb58
Krirll <balalaev20022@gmail.com>
2021-08-18 16:31:50 +0300

    практическая №7

---
+++ PracticAndroid7/app/src/main/java/com/example/practicandroid7/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.practicandroid7
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("SetTextI18n")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val button = findViewById<Button>(R.id.button)
+        // ax + b < 0
+        button.setOnClickListener {
+            val first = findViewById<EditText>(R.id.firstNumber).text.toString()
+            val second = findViewById<EditText>(R.id.secondNumber).text.toString()
+            val result = findViewById<TextView>(R.id.textView)
+            if (first.toIntOrNull() != null && second.toIntOrNull() != null) {
+                if (first.toInt() == 0) {
+                    if (second.toInt() < 0) result.text = "x - любое число"
+                    else result.text = "нет решений"
+                }
+                else result.text = "x < ${(-second.toInt() / first.toInt())}"
+            }
+            else result.text = resources.getString(R.string.error)
+        }
+    }
+}
