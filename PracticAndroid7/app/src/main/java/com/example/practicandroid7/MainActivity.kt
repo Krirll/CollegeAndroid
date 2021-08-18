03e2dadc388fb8445adf261cc57864a4e804265f
Krirll <balalaev20022@gmail.com>
2021-08-18 23:39:05 +0300

    добавил новый Activity для вывода результата

--- PracticAndroid7/app/src/main/java/com/example/practicandroid7/MainActivity.kt
+++ PracticAndroid7/app/src/main/java/com/example/practicandroid7/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.example.practicandroid7
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
-import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,15 +15,19 @@ class MainActivity : AppCompatActivity() {
         button.setOnClickListener {
             val first = findViewById<EditText>(R.id.firstNumber).text.toString()
             val second = findViewById<EditText>(R.id.secondNumber).text.toString()
-            val result = findViewById<TextView>(R.id.textView)
-            if (first.toDoubleOrNull() != null && second.toDoubleOrNull() != null) {
-                if (first.toDouble() == 0.0) {
-                    if (second.toDouble() < 0) result.text = resources.getString(R.string.result1)
-                    else result.text = resources.getString(R.string.result2)
-                }
-                else result.text = resources.getString(R.string.resultOfEquation, String.format("%.3f", (-second.toDouble() / first.toDouble())))
-            }
-            else result.text = resources.getString(R.string.error)
+            val result =
+                if (first.toDoubleOrNull() != null && second.toDoubleOrNull() != null) {
+                    if (first.toDouble() == 0.0) {
+                        if (second.toDouble() < 0) resources.getString(R.string.result1)
+                        else resources.getString(R.string.result2)
+                    } else resources.getString(
+                        R.string.resultOfEquation,
+                        String.format("%.3f", (-second.toDouble() / first.toDouble()))
+                    )
+                } else resources.getString(R.string.error)
+            val intent = Intent(this, ResultActivity::class.java)
+            intent.putExtra("RESULT", result)
+            startActivity(intent)
         }
     }
 }
---
+++ PracticAndroid7/app/src/main/java/com/example/practicandroid7/ResultActivity.kt
@@ -0,0 +1,14 @@
+package com.example.practicandroid7
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class ResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+        findViewById<TextView>(R.id.textView).text =
+            intent.getStringExtra("RESULT")
+    }
+}
