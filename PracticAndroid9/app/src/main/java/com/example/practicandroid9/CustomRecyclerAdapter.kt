535b86c9c0280d928ca8490f565fac3fc9fe91a8
Krirll <balalaev20022@gmail.com>
2021-08-30 22:28:22 +0300

    сделал удаление одного элемента и обновление экрана после удаления

--- PracticAndroid9/app/src/main/java/com/example/practicandroid9/CustomRecyclerAdapter.kt
+++ PracticAndroid9/app/src/main/java/com/example/practicandroid9/CustomRecyclerAdapter.kt
@@ -1,15 +1,18 @@
 package com.example.practicandroid9
 
 import android.app.AlertDialog
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import java.text.SimpleDateFormat
-import java.util.*
+import java.util.Locale
 
-class CustomRecyclerAdapter(private val list : MutableList<Objects>) : RecyclerView.Adapter<CustomRecyclerAdapter.MyViewHolder>() {
+class CustomRecyclerAdapter(private val list : MutableList<Objects>, private val recyclerView: RecyclerView) :
+    RecyclerView.Adapter<CustomRecyclerAdapter.MyViewHolder>() {
+
     class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         var product: TextView = itemView.findViewById(R.id.product)
         var count: TextView = itemView.findViewById(R.id.countOfProduct)
@@ -32,10 +35,14 @@ class CustomRecyclerAdapter(private val list : MutableList<Objects>) : RecyclerV
         holder.itemView.setOnLongClickListener {
             val dialogBuilder = AlertDialog.Builder(holder.itemView.context)
             dialogBuilder.setCancelable(false)
-                .setPositiveButton(R.string.delete) { dialog, _ -> dialog.dismiss(); Elements.delete(holder.adapterPosition) }
-                //todo удалить элемент из списка и обновить RecyclerView
-                .setNegativeButton(R.string.edit) { dialog, _  -> dialog.dismiss() }
-                //todo переходить на экран редактирования
+                .setPositiveButton(R.string.delete) { dialog, _ -> dialog.dismiss()
+                                                                    Elements.delete(holder.adapterPosition)
+                                                                    recyclerView.adapter?.notifyItemRemoved(holder.adapterPosition)
+                }
+                .setNegativeButton(R.string.edit) { dialog, _  -> dialog.dismiss()
+                                                                    val intent = Intent(recyclerView.context, AddElementActivity::class.java)
+                                                                    
+                }
                 .setNeutralButton(R.string.cancel) { dialog, _  -> dialog.dismiss() }
             val alert = dialogBuilder.create()
             alert.setTitle(R.string.ChooseAction)
--- PracticAndroid9/app/src/main/java/com/example/practicandroid9/ShowElementsActivity.kt
+++ PracticAndroid9/app/src/main/java/com/example/practicandroid9/ShowElementsActivity.kt
@@ -2,11 +2,8 @@ package com.example.practicandroid9
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import java.text.SimpleDateFormat
-import java.util.*
 
 class ShowElementsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,6 +11,6 @@ class ShowElementsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_show_elements)
         val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
         recyclerView.layoutManager = LinearLayoutManager(this)
-        recyclerView.adapter = CustomRecyclerAdapter(Elements.printAll())
+        recyclerView.adapter = CustomRecyclerAdapter(Elements.printAll(), recyclerView)
     }
 }
