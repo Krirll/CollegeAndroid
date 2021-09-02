68021b2591cb8431bbc3a2e0285d5d1695bb0986
Krirll <balalaev20022@gmail.com>
2021-09-02 20:47:57 +0300

    попытки реализации ToolBar

--- PracticAndroid9/app/src/main/java/com/example/practicandroid9/ShowElementsActivity.kt
+++ PracticAndroid9/app/src/main/java/com/example/practicandroid9/ShowElementsActivity.kt
@@ -2,6 +2,8 @@ package com.example.practicandroid9
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -12,5 +14,21 @@ class ShowElementsActivity : AppCompatActivity() {
         val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
         recyclerView.layoutManager = LinearLayoutManager(this)
         recyclerView.adapter = CustomRecyclerAdapter(Elements.printAll(), recyclerView)
+        //setting toolbar
+        setSupportActionBar(findViewById(R.id.toolbar))
+    }
+
+    //setting menu in action bar
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_for_toolbar,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    // actions on click menu items
+    override fun onOptionsItemSelected(item: MenuItem): Boolean = when (item.itemId) {
+        R.id.addBarButton -> { true }
+        R.id.searchBarButton -> { true }
+        R.id.choiceSortBarButton -> { true }
+        else -> { false }
     }
 }
